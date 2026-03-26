6442efea740dddaa3692cff0d8a4b3793579d98f
YZYLAB MAC <general@yzylab.com>
2026-03-26 12:15:16 +0100

    Added Solana Tracker

---
+++ src/platforms/solanatracker.ts
@@ -0,0 +1,19 @@
+import { PlatformRaw, ServiceRaw } from "../types";
+
+
+export const platform: PlatformRaw = {
+  id: "solanatracker",
+  name: "Solana Tracker",
+  description:
+    "Solana Tracker offers Data APIs, RPC Services, Swap API and a trading platform.",
+  links: {
+    website: "https://www.solanatracker.io/",
+    discord: "https://discord.gg/pzW6wMfsJT",
+    twitter: "https://x.com/solanatrackr",
+    github: "https://github.com/solanatrackr",
+  },
+  tags: ["tool", "infrastructure"],
+};
+
+
+export const services: ServiceRaw[] = [];
